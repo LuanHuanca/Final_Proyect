37d648c3f1cf1df00dbc74a6d42c68e8780b3664
Mari <isabelvela096@gamil.com>
2023-06-11 21:01:23 -0400

    commit2 Vela

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputContainer from '../Componentes/InputContainer.jsx'
 import Title from '../Componentes/Title.jsx'
 import Button from '../Componentes/Button.jsx'
 import './Login.css'
 import { NavLink } from 'react-router-dom'
+import Validation from'./LoginValidation.jsx';
 
 const Login = () => {
+  const [values, setValues] = useState ({
+      email: '',
+      password: ''
+  })
+  const [errors, setErrors] = useState({})
+  const handleInput = (event) => {
+    setValues(prev => ({...prev, [event.target.name] : [event.target.value] }))
+  }
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    setErrors(Validation(values));
+
+  }
   return (
     <div className='box'>
         <Title title={"INICIO DE SESION"} context={"Inicia sesión con tu correo electrónico"}/>
-        <InputContainer title={"Email"} type={"text"} shadow={"example@gmail.com"}/>
-        <InputContainer title={"Contraseña"} type={"password"} shadow={"Introduce tu contraseña"}/>
-        <NavLink to={"/Profile"}><Button name={"Inicia Sesión"}/></NavLink>
-        <h5 className='text-h5'>No Tienes Cuenta? <NavLink to={"/register"}>Registrate</NavLink></h5>
+        <form action='' onSubmit={handleSubmit}>
+          <div>
+            <InputContainer title={"Email"} type={"email"} shadow={"example@gmail.com"} name='email'
+            onChange={handleInput}/>
+            {errors.email && <span className='text-danger'>  {errors.email}</span>}
+          </div>
+          <div>
+            <InputContainer title={"Contraseña"} type={"password"} shadow={"Introduce tu contraseña"} name='password'
+            onChange={handleInput}/>
+            {errors.password && <span className='text-danger'>  {errors.password}</span>}
+          </div>
+            <Button  type='submit' name={"Inicia Sesión"}/>
+            <NavLink to={"/Profile"}></NavLink>
+            <h5 className='text-h5'>No Tienes Cuenta? <NavLink to={"/register"}>Registrate</NavLink></h5>
+        </form>
     </div>
   )
 }
--- src/Screens/Register.jsx
+++ src/Screens/Register.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Register.css'
 import InputContainer from '../Componentes/InputContainer.jsx'
 import Title from '../Componentes/Title.jsx'
 import Button from '../Componentes/Button.jsx'
 import { NavLink } from 'react-router-dom'
+import Validation from './RegisterValidation';
+import axios from 'axios'
+
 
 const Register = () => {
+  const [values, setValues] = useState ({
+    name : '',
+    email: '',
+    password: ''
+})
+const [errors, setErrors] = useState({})
+const handleInput = (event) => {
+  setValues(prev => ({...prev, [event.target.name] : [event.target.value] }))
+}
+
+const handleSubmit = (event) =>{
+  event.preventDefault();
+  setErrors(Validation(values));
+  if(errors.name === "" && errors.email === "" && errors.password === ""){
+     axios.post('')
+  }
+
+}
   return (
     <div >
         <Title title={"REGISTRO"} context={"Llene los siguientes datos"}/>
-        <InputContainer title='Nombre Completo' type={"text"} shadow={"Josue Romay Torrejon"}/>
-        <InputContainer title='Email' type={"text"} shadow={"example@gmail.com"}/>
-        <InputContainer title='Contraseña' type={"password"} shadow={"Introduce tu contraseña"}/>
-        <NavLink to={"/login"}><Button name={"Crear Cuenta"}/></NavLink>
+        <form action='' onSubmit={handleSubmit}>
+        <div>
+        <InputContainer title='Nombre Completo' type={"text"} shadow={"Josue Romay Torrejon"} name='name'
+        nChange={handleInput}/>
+        {errors.name && <span className='text-danger'>  {errors.name}</span>}
+        </div>
+        <div>
+        <InputContainer title='Email' type={"text"} shadow={"example@gmail.com"} name='email'
+        nChange={handleInput}/>
+        {errors.email && <span className='text-danger'>  {errors.email}</span>}
+        </div>
+        <div>
+        <InputContainer title='Contraseña' type={"password"} shadow={"Introduce tu contraseña"} name='password'
+        nChange={handleInput}/>
+        {errors.password && <span className='text-danger'>  {errors.password}</span>}
+        </div>
+        <Button type='submit' name={"Crear Cuenta"}/>
+        <NavLink to={"/login"}></NavLink>
+        
         <h5 className='text-h5'>Ya Tienes una Cuenta? <NavLink to={"/login"}>Inicia Sesion</NavLink></h5>
+        </form>
     </div>
   )
 }
