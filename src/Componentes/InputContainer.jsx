d5d73467c98c195ae3eb1c9101dcfcdd68fa53de
Luis Huanca <luanhuancam@gmail.com>
2023-06-12 19:31:31 -0400

    sin cambios

--- src/Componentes/InputContainer.jsx
+++ src/Componentes/InputContainer.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './InputContainer.css'
 
-const InputContainer = ({title, type, shadow}) => {
+const InputContainer = ({title, type, shadow,action}) => {
   return (
     <div className='container'>
       <h4>{title}</h4>
       <div className='input-container'>
-        <input type={type} placeholder={shadow}/>
+        <input 
+        type={type}
+        placeholder={shadow}
+        onChange={action}
+        />
       </div>
     </div>
   )
