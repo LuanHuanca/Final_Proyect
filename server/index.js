ed6a368872e946097167fef9339f560be2192eb3
Luis Huanca <luanhuancam@gmail.com>
2023-06-12 01:51:53 -0400

    indexjs in comment

--- server/index.js
+++ server/index.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const app = express();
-const mysql = require("mysql");
-const cors = require ("cors");
+// const express = require("express");
+// const app = express();
+// const mysql = require("mysql");
+// const cors = require ("cors");
 
-app.use(cors());
-app.use(express.json())
+// app.use(cors());
+// app.use(express.json())
 
-//Coneccion a la base de datos
-const database = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"db_chifa"
-});
-
-
-app.post("/register",(request,res)=>{
-    const ci = request.body.ci;
-    const username = request.body.username;
-    const password = request.body.password;
-    const email = request.body.email;
+// //Coneccion a la base de datos
+// const database = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"",
+//     database:"db_chifa"
+// });
 
 
-    database.query('INSERT INTO cliente (id_ci_cliente,username,contrasena,email) VALUES (?,?,?,?)', [ci,username,password,email],
-    (error,result)=>{
-        if(error){
-            console.log(error);
-        }else{
-            res.send("Cliente Registrado con exito!!!");
-        }
-    }
-    );
-});
+// app.post("/register",(request,res)=>{
+//     const ci = request.body.ci;
+//     const username = request.body.username;
+//     const password = request.body.password;
+//     const email = request.body.email;
 
-app.listen(5173,()=>{
-    console.log("Corriendo en el puerto 5173")
-})
+
+//     database.query('INSERT INTO cliente (id_ci_cliente,username,contrasena,email) VALUES (?,?,?,?)', [ci,username,password,email],
+//     (error,result)=>{
+//         if(error){
+//             console.log(error);
+//         }else{
+//             res.send("Cliente Registrado con exito!!!");
+//         }
+//     }
+//     );
+// });
+
+// app.listen(5173,()=>{
+//     console.log("Corriendo en el puerto 5173")
+// })
