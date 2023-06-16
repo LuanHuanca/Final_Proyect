cb848a79b5874bb2825be5b5b4fa9301a44e9b71
Mari <isabelvela096@gamil.com>
2023-06-16 00:55:16 -0400

    vela

--- src/main.jsx
+++ src/main.jsx
@@ -14,15 +14,9 @@ import Reviews from './Screens/Reviews.jsx'
 import Profile from './Screens/Profile'
 import ProfileDirection from './Screens/ProfileDirection'
 import GeneralReserve  from './Screens/GeneralReserve.jsx'
-<<<<<<< HEAD
-import GeneralDetails  from './Screens/GeneralDetails.jsx'
-import GeneralConfirm from './Screens/GeneralConfirm.jsx'
-import Menu from './Screens/Menu.jsx'
-=======
 import App from './App'
 import Unreserve from './Screens/Unreserve'
 import Reserved from './Screens/Reserved'
->>>>>>> 9700b46dc12b096e4ed9a778bd5a8e95a192b4b3
 
 
 
@@ -39,18 +33,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route  path='/reserve' element={<Reserve/>}/>
         <Route  path='/reviews' element={<Reviews/>}/>
         <Route  path='/general/reserve' element={<GeneralReserve/>}/>
-<<<<<<< HEAD
-        <Route  path='/general/details' element={<GeneralDetails/>}/>
-        <Route  path='/general/confirm' element={<GeneralConfirm/>}/>
-        <Route path='/menu' element={<Menu/>}/>
-        
-       
-=======
         <Route  path='/app' element={<App/>}/>
         <Route  path='/profile' element={<Profile/>}/>
         <Route  path='/unreserved' element={<Unreserve/>}/>
         <Route  path='/reserved' element={<Reserved/>}/>
->>>>>>> 9700b46dc12b096e4ed9a778bd5a8e95a192b4b3
       </Routes>
     <NavBar/>
     </BrowserRouter>
